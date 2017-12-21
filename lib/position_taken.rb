88134c5e38aec97aa330c2b5ca6ff40460886353
William Thompson <wmthmps1@gmail.com>
2017-12-21 21:04:26 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,8 @@
 # code your #position_taken? method here!
 board=[" "," "," "," "," "," "," "," "," "]
 def position_taken?(board,index)
-  
+  if board[0] == " " || "" || nil
+    return false
+  end
+elsif
 end
