82e87b419f5ac001065816fe71e05ed314139da7
William Thompson <wmthmps1@gmail.com>
2017-12-21 21:11:17 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,4 @@ def position_taken?(board,index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
   end
- else board[index] == "X" || board[index] == "O"
-    return true
- end
 end
